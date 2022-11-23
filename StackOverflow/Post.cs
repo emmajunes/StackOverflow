3d73579b8633f9a53836c9df3f5ef8097b40782a
DESKTOP-9FQ8PPR\Emma Junes <emmaastrom91@gmail.com>
2022-11-23 12:38:38 +0100

    Add project files.

---
+++ StackOverflow/Post.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+
+namespace StackOverflow
+{
+    internal partial class Program
+    {
+        public class Post
+    {
+            public string _title { get; set; }
+            public string _description { get; set; }
+            public DateTime _time { get; set; }
+
+            private int vote = 0;
+
+            private int sum = 0;
+
+           
+            public DateTime DateTime()
+            {
+                return _time;
+            }
+
+            public Post(string title, string description, DateTime time) 
+            {
+                _title = title;
+                _description = description;
+                _time = time;
+                
+            }
+            public void upVote()
+            {
+                vote++;
+
+            }
+
+            public void downVote()
+            {
+                vote--;
+            }
+
+            public void getVote()
+            {
+                sum += vote;
+                Console.WriteLine("Your post has " + sum + " votes");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ StackOverflow/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StackOverflow
+{
+    internal partial class Program
+    {    
+        static void Main(string[] args)
+        {
+            var post = new Post("Hej","Goddag",DateTime.Now);
+
+            Console.WriteLine(post._title + " " + post._description + " " + post._time);
+
+            
+            post.upVote();
+            post.upVote();
+            post.upVote();
+            post.downVote();
+
+            post.getVote();
+   
+        }
+    }
+}
